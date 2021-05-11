1f2022392d8a553350a6a9e17d467aaa7e0a04cc
EnriqueMC557 <kikemeca05@gmail.com>
2021-05-11 13:28:18 -0500

    Prueba DAQ Developer terminada. Implementada con threads. La hora sigue siendo la local debido a que World Time API está saturada pero igual está listo para consultar la hora de Japón. Faltan detalles estéticos en la gráfica.

--- DataBase.py
+++ DataBase.py
@@ -6,6 +6,7 @@ import requests
 
 class RookDataBase:
 	def __init__(self):
+		# Inicia conexión con BDD
 		self.connection = pymysql.connect(
 			host = 'localhost',
 			user = 'emc',
@@ -16,11 +17,14 @@ class RookDataBase:
 		print('Conexión exitosa')
 	
 	def close(self):
+		# Termina conexión con BDD
 		self.connection.close()		
 		print('Conexión terminada')
 	
 	def insertTemperature(self):
+		# Inserta temperatura y timestamp en BDD
 		self.localTime()
+		#self.JapanTime() # Descomentar para utilizar tiempo de Japón 
 		self.temperature()
 		sql = f"""INSERT INTO data (timestamp, variable_name, value)
 			  VALUES ('{self.time}', 'temperature', {self.temp})"""
@@ -28,7 +32,9 @@ class RookDataBase:
 		self.connection.commit()
 
 	def insertCPUload(self):
+		# Inserta carga de CPU y timestamp en BDD
 		self.localTime()
+		#self.JapanTime() # Descomentar para utilizar tiempo de Japón 
 		self.load()
 		sql = f"""INSERT INTO data (timestamp, variable_name, value)
 			  VALUES ('{self.time}', 'load', {self.loadCPU})"""
@@ -36,41 +42,45 @@ class RookDataBase:
 		self.connection.commit()
 		
 	def select_all(self):
+		# Consulta en BDD los valores de temperatura y carga de CPU
 		self.localTime()
+		#self.JapanTime() # Descomentar para utilizar tiempo de Japón
+		
 		sql_1 = f"""SELECT value FROM data WHERE
 				variable_name = 'temperature' AND
 				timestamp >= DATE_SUB('{self.time}',INTERVAL 8 HOUR)"""
 		sql_2 = f"""SELECT value FROM data WHERE
 				variable_name = 'load' AND
 				timestamp >= DATE_SUB('{self.time}',INTERVAL 8 HOUR)"""		
-		try:
-			self.cursor.execute(sql_1)
-			self.temperatureValues = self.cursor.fetchall()
-			
-			self.cursor.execute(sql_2)
-			self.loadValues = self.cursor.fetchall()
-				
-		except Exception as e:
-			raise
+		
+		self.cursor.execute(sql_1)
+		self.temperatureValues = self.cursor.fetchall()
+		
+		self.cursor.execute(sql_2)
+		self.loadValues = self.cursor.fetchall()
 	
 	def JapanTime(self):
+		# Consulta hora en Japón utilizando World Time API
 		response = requests.get('http://worldtimeapi.org/api/timezone/Asia/Tokyo')
 		time = response.json()['datetime']
 		time = datetime.datetime.fromisoformat(time).strftime('%Y-%m-%d %H:%M:%S')
 		self.time = time
 
 	def localTime(self):
+		# Consulta hora del sistema
 		now = time.time()
 		now = datetime.datetime.fromtimestamp(now).strftime('%Y-%m-%d %H:%M:%S')
 		self.time = now
 		
 	def temperature(self):
+		# Consulta temperatura del sistema
 		temperatureFile = open('/sys/class/thermal/thermal_zone0/temp')
 		temperature = float(temperatureFile.read())
 		temperatureFile.close()
 		self.temp = temperature/1000
 		
 	def load(self):
+		# Consulta carga de CPU
 		load = psutil.cpu_percent()
 		self.loadCPU = load
 
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
+import time
 from PruebaRook import Ui_Form
 from DataBase import RookDataBase
 from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtCore import QTimer, QThread
+from PyQt5.QtCore import QThread, pyqtSignal
 
 class gui(QWidget):
     def __init__(self, parent=None):
@@ -12,33 +13,34 @@ class gui(QWidget):
         self.ui.setupUi(self)
         self.figure = self.ui.PlotCanvas.canvas
         self.BDD = RookDataBase()
-
-        # Inicializador de QTimer
-        self.timer = QTimer()
-        self.timer.start(60000) # Reinicio cada 1 minuto (60 segundos)
-
-        # Conexion de señal de Qtimer con slot
-        self.timer.timeout.connect(self.cambioHora)
-        self.timer.timeout.connect(self.cambioTemperatura)
-        self.timer.timeout.connect(self.cambioCarga)
+        
+        # Inicialización de thread para temperatura
+        self.thread1 = Thread1()
+        self.thread1.newTimeTemperature.connect(self.cambioTemperatura)
+        self.thread1.start()
+        
+        # Inicialización de thread para carga
+        self.thread2 = Thread2()
+        self.thread2.newTimeLoad.connect(self.cambioCarga)
+        self.thread2.start()
 
         # Conexion de señal de botón con slot
         self.ui.PlotButton.clicked.connect(self.plotData)
 
-    def cambioHora(self):
-        self.BDD.localTime()
-        self.ui.TimeText.setText(f'{self.BDD.time}')
+    def cambioTemperatura(self, time, temperature):
+        # Cambia texto de tiempo y temperatura
+        self.ui.TimeText.setText(time)
+        self.ui.TemperatureText.setText(temperature)
 
-    def cambioTemperatura(self):
-        self.BDD.insertTemperature()
-        self.ui.TemperatureText.setText(f'{self.BDD.temp}')
-
-    def cambioCarga(self):
-        self.BDD.insertCPUload()
-        self.ui.LoadText.setText(f'{self.BDD.loadCPU}')
+    def cambioCarga(self, time, load):
+        # Cambia texto de tiempo y carga
+        self.ui.TimeText.setText(time)
+        self.ui.LoadText.setText(load)
 
     def plotData(self):
+        # Consulta datos de temperatura y carga de las ultimas 8 hrs
         self.BDD.select_all()
+        # Grafica los datos
         self.figure.ax.clear()
         self.figure.ax.plot(self.BDD.temperatureValues)
         self.figure.ax.plot(self.BDD.loadValues)
@@ -47,14 +49,48 @@ class gui(QWidget):
         self.figure.draw()
         
     def closeEvent(self, event):
+        # Termina conexiones con BDD
         self.BDD.close()
+        self.thread1.BDD.close()
+        self.thread2.BDD.close()
+        
 
+class Thread1(QThread):
+    # Señal para envío de tiempo y temperatura
+    newTimeTemperature = pyqtSignal(str, str)
+    # Conexión a BDD
+    BDD = RookDataBase()
+        
+    def run(self):
+        while True:
+            # Espera 1 minuto (60 segundos)
+            time.sleep(60)
+            # Consulta e inserta tiempo y temperatura
+            self.BDD.insertTemperature()
+            # Envía señal con tiempo y temperatura
+            self.newTimeTemperature.emit(self.BDD.time[11:], str(self.BDD.temp))
+
+class Thread2(QThread):
+    # Señal para envío de tiempo y carga
+    newTimeLoad = pyqtSignal(str, str)
+    # Conexión a BDD
+    BDD = RookDataBase()
+    
+    def run(self):
+        while True:
+            # Espera 1 minuto (60 segundos)
+            time.sleep(60)
+            # Consulta e inserta tiempo y carga
+            self.BDD.insertCPUload()
+            # Envía señal con tiempo y carga
+            self.newTimeLoad.emit(self.BDD.time[11:], str(self.BDD.loadCPU))
+                
 if __name__ == '__main__':
-    # crea la instancia de la aplicación
+    # Crea la instancia de la aplicación
     app = QApplication(sys.argv)
-    # crea la instancia de la GUI
+    # Crea la instancia de la GUI
     gui = gui()
-    # nuestra la GUI
+    # Muestra la GUI
     gui.show()
-    # ejecuta la aplicación
+    # Ejecuta la aplicación
     sys.exit(app.exec_())
